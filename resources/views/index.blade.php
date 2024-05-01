e115b8b754df3ef0f165bddb66eed2c2af36e1d3
yossrmohammed <yossr.khatab5@gmail.com>
2024-05-01 18:20:42 +0300

    install carbon adn format created at date

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,44 +1,43 @@
 @extends('layouts.app')
 
 @section("content")
-<a href="{{route('post.create')}}" class="btn btn-dark">Create Post </a>
-    <h1 style="background-color: white;"> All posts </h1>
-    <table class='table'> <tr> <td>ID </td> <td> title</td>
-            <td>body</td> <td> Actions </td><td> posted by </td></tr>
-        @foreach($posts as $post)
-            <tr>
-                <td> {{$post->id}}</td>
-                <td> {{$post->title}}</td>
-                <td> {{$post->body}}</td>
-                <td> {{$post->posted_by}}</td>
-
-
-                <td> <a href="{{route('post.show',$post['id'] )}}" class="btn btn-info">Show  </a></td>
-                <td> <a href="{{route('post.edit',$post['id'] )}}" class="btn btn-info">Edit  </a></td>
-                <td>
-                    <td>
-                        <form id="delete-form" action="{{ route('post.destroy', ['id' => $post['id']]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="button" class="btn btn-danger" onclick="confirmDelete()">Delete</button>
-                           
-                        </form>
-                    </td>
-                    
-                </td>
-                
-
-            </tr>
-
-        @endforeach
-
-    </table>
-    {{$posts ->links()}}
+<a href="{{ route('post.create') }}" class="btn btn-dark">Create Post</a>
+<h1 style="background-color: white;">All posts</h1>
+<table class='table'>
+    <tr>
+        <td>ID</td>
+        <td>Title</td>
+        <td>Body</td>
+        <td>Posted By</td>
+        <td>Created At</td>
+        <td>Actions</td>
+    </tr>
+    @foreach($posts as $post)
+    <tr>
+        <td>{{ $post->id }}</td>
+        <td>{{ $post->title }}</td>
+        <td>{{ $post->body }}</td>
+        <td>{{ $post->posted_by }}</td>
+        <td>{{ $post->created_at->format('Y/m/d H:i') }}</td> <!-- Format created_at using Carbon -->
+        <td>
+            <a href="{{ route('post.show', $post->id) }}" class="btn btn-info">Show</a>
+            <a href="{{ route('post.edit', $post->id) }}" class="btn btn-info">Edit</a>
+            <form id="delete-form-{{ $post->id }}" action="{{ route('post.destroy', $post->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="button" class="btn btn-danger" onclick="confirmDelete('{{ $post->id }}')">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+{{ $posts->links() }}
 @endsection
+
 <script>
-    function confirmDelete() {
+    function confirmDelete(postId) {
         if (confirm("Are you sure you want to delete this post?")) {
-            document.getElementById('delete-form').submit();
+            document.getElementById('delete-form-' + postId).submit();
         }
     }
 </script>
