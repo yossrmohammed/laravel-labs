fd909da77399d72b4fa3b976e7c9629e130a273a
yossrmohammed <yossr.khatab5@gmail.com>
2024-05-03 14:06:45 +0300

    add PruneOldPostsJob

---
+++ app/Jobs/PruneOldPostsJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
+
+class PruneOldPostsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $oneHourAgo = Carbon::now()->subHour();
+
+        Post::where('created_at', '<', $oneHourAgo)->delete();
+    }
+}
